605493f73f626321f151fa4e92c5d1c0ec7f6db2
ing.conti <ingconti@ingconti.com>
2023-05-12 17:56:45 +0200

    cleaned up

--- src/main/java/com/ingconti/HelloApplication.java
+++ src/main/java/com/ingconti/HelloApplication.java
@@ -18,22 +18,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        /*
-        for (String arg: args) {
-            System.out.println(arg);
-        }
-        */
-        if (args.length > 0) {
-            String param0 = args[0];
-            if (param0.equals( "--server") )
-                runAsServer();
-        }else {
             launch();
         }
     }
-
-    static void runAsServer(){
-        System.out.println("started as server.. bye!");
-    }
-
-}
